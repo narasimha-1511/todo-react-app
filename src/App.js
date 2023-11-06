95c462f95cee6df0ed9374993361e50a9edc9ff0
narasimha-1511 <129654598+narasimha-1511@users.noreply.github.com>
2023-11-06 17:40:59 +0530

    Basic Version of Todo Using React App
    Signed-off-by: narasimha-1511 <129654598+narasimha-1511@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,133 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+function Square({ value, onSquareClick }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
   );
 }
 
-export default App;
+export default function Board() {
+  const [xisNext, setXisNext] = useState(true);
+  const [squares, Setsquares] = useState(Array(9).fill("_"));
+
+  function handleClick(i) {
+    if (squares[i] == "X" || squares[i] == "O") return;
+
+    const newSquares = squares.slice();
+    if (xisNext) {
+      newSquares[i] = "X";
+    } else {
+      newSquares[i] = "O";
+    }
+    Setsquares(newSquares);
+    setXisNext(!xisNext);
+  }
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [0, 3, 6],
+      [3, 4, 5],
+      [6, 7, 8],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        if (squares[a] != "_") return squares[a];
+      }
+    }
+
+    return null;
+  }
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xisNext ? "X" : "O");
+  }
+
+  return (
+    <>
+      <div className="outer">
+        <h3>
+          <u>Tic-Tac-Toe</u>
+          <br /> {status}
+        </h3>
+        <div className="TodoCenter">
+          <div className="board-row">
+            <Square
+              value={squares[0]}
+              onSquareClick={() => {
+                handleClick(0);
+              }}
+            />
+            <Square
+              value={squares[1]}
+              onSquareClick={() => {
+                handleClick(1);
+              }}
+            />
+            <Square
+              value={squares[2]}
+              onSquareClick={() => {
+                handleClick(2);
+              }}
+            />
+          </div>
+          <div className="board-row">
+            <Square
+              value={squares[3]}
+              onSquareClick={() => {
+                handleClick(3);
+              }}
+            />
+            <Square
+              value={squares[4]}
+              onSquareClick={() => {
+                handleClick(4);
+              }}
+            />
+            <Square
+              value={squares[5]}
+              onSquareClick={() => {
+                handleClick(5);
+              }}
+            />
+          </div>
+          <div className="board-row">
+            <Square
+              value={squares[6]}
+              onSquareClick={() => {
+                handleClick(6);
+              }}
+            />
+            <Square
+              value={squares[7]}
+              onSquareClick={() => {
+                handleClick(7);
+              }}
+            />
+            <Square
+              value={squares[8]}
+              onSquareClick={() => {
+                handleClick(8);
+              }}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
